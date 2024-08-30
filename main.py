d51d5e6eb0d934f1d0178ecd595260fd23677fd0
ZIGZOZAY <103761530+zigzozay@users.noreply.github.com>
2024-08-31 00:20:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ from discord import app_commands
 
 import os
 
-port = int(os.getenv("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+# main.py
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    return app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
 
 
 from dotenv import load_dotenv
